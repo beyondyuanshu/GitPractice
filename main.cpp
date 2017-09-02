5d55a06880a7edc67cc331aa29e5e021b9764cb0
yuanshu <yuanshu@detu.com>
2017-09-02 15:52:38 +0800

    fdsfas

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include "gitpractice.h"
 #include <QtWidgets/QApplication>
+#include <QDebug>
+#include <QList>
+#include <QMap>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+	QString str = QObject::tr("Hello Qt");
+	
+	QMap<QString, QString> map;
+	map.insert("Hello", "Qt");
+	map.insert("Hello", "World");
+
+	qDebug() << "test";
+
+	qDebug() << "Test merge";
+
 	GitPractice w;
 	w.show();
 	return a.exec();
